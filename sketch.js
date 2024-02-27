21ff3822248b092b17598daf8689164dd61d3860
Brody Mennitto <brodymennitto@Brodys-Air.ad.pcc.edu>
2024-02-27 14:22:22 -0800

    working on it

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
+let particles = [];
+
 function setup() {
   createCanvas(400, 400);
-}
+  }
 
 function draw() {
-  background(220);
+  background(0);
+  let p = new Particle();
+  particles.push(p);
+  for (let i = 0; i < particles.length; i++){
+  particles[i].update();
+  particles[i].show();
+ }
 }
+
+class Particle {
+  constructor(){
+    this.x = width/2;
+    this.y = 390;
+    this.vx = random(-1, 1);
+    this.vy = random(-5, -1);
+  }
+
+  update() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+show() {
+  noStroke();
+  fill(255, 50);
+  rectMode(CENTER);
+  rect(this.x, this.y, 40);
+  
+}
+}
+
